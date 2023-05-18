af3fdcbcc11f755727ae3a9ae2641aa3ba51919e
Gary <christg7@gmatc.matc.edu>
2023-05-17 23:07:54 -0500

    updating work

---
+++ Project-1/js/scripts.js
@@ -0,0 +1,31 @@
+const Playstation = ["Ark Survival Evovled", "Warframe", "Elden Ring", "COD Modern Warfare"];
+const Switch = ["Pokemon Violet", "Animal Crossing New Horizons","Super Smash Brothers Ultimate", "Sega Mega Drive Collection"];
+const PC = ["Cosmoteer", "Factorio", "Homeworld", "Timberborn"];
+const AgeRating = ["E", "E10+", "T", "M"];
+const Own = ["Physical", "Disc", "Both", "N/A"];
+
+document.getElementById("PS").innerHTML = Playstation[index];
+for (let index = 0; index < array.length; index++) {
+    const Playstation = Playstation[index];
+    
+}
+document.getElementById("S").innerHTML = Switch[index];
+for (let index = 0; index < array.length; index++) {
+    const Switch = Switch[index];
+    
+}
+document.getElementById("PC").innerHTML = PC[index];
+for (let index = 0; index < array.length; index++) {
+    const PC = PC[index];
+    
+}
+document.getElementById("AR").innerHTML = AgeRating[index];
+for (let index = 0; index < array.length; index++) {
+    const document.getElementById("AR").innerHTML = AgeRating[index];
+    
+}
+document.getElementById("O").innerHTML = Own[index];
+for (let index = 0; index < array.length; index++) {
+    const Own = Own[index];
+    
+}
---
+++ Project-2/js/scripts.js
@@ -0,0 +1,17 @@
+const Questions = ["For paragraphs you mainly use Which element?","How do you create a division with an element?",
+"What is the purpose of the numbers in the h elements?","Which of these is not a Selector in CSS",
+"Which of these is not a datatype in JavaScript?","How do you do a Multi-line comment in javascript","Which attribute do you give an event to allow a Html element to be clicked?"];
+
+const Answers1 = ["d","div","To organize headers in Order",".class","Booleaan","*//*","Click"];
+
+const Answers2 = ["r","br","Change the font Size of headers based importance","#id","Int","/**/","Onclick"];
+
+const Answers3 = ["Par","divide","To change the style of headers","*","Number","//","Clickon"];
+
+const Answers4 = ["p","d","There is no purpose for the numbers","//","Null","*\\*","Clickable"];
+
+document.getElementById("Question").innerHTML = Questions[0];
+document.getElementById("A1").innerHTML = Answers1[0];
+document.getElementById("A2").innerHTML = Answers2[0];
+document.getElementById("A3").innerHTML = Answers3[0];
+document.getElementById("A4").innerHTML = Answers4[0]; 
